fdd825e1fbe52d5ccbb7a923952406673dc65526
Yuval Herman <yuvalherman99@gmail.com>
2024-03-04 10:00:25 +0200

    added raylib to test build

--- build.zig
+++ build.zig
@@ -84,6 +84,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    exe_unit_tests.addIncludePath(.{ .path = "raylib/src" });
+    exe_unit_tests.linkLibrary(raylib catch unreachable);
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
