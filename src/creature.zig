968b37e331b9056fd9ff5fe04c78152071ae675b
Yuval Herman <yuvalherman99@gmail.com>
2024-03-07 19:54:59 +0200

    remade creature

---
+++ src/creature.zig
@@ -0,0 +1,96 @@
+const std = @import("std");
+const r = @import("cHeaders.zig").raylib;
+const Allocator = std.mem.Allocator;
+const assert = std.debug.assert;
+
+var prng = std.Random.DefaultPrng.init(3);
+const random = prng.random();
+
+pub fn init(groundLevel: comptime_int, gravity: comptime_float) type {
+    const Physics = @import("physics/particle.zig").init(groundLevel, gravity);
+    const Particle = Physics.Particle;
+    const Spring = Physics.ParticleSpring;
+    return struct {
+        pub const Creature = struct {
+            joints: []Particle,
+            connections: []Spring,
+
+            pub fn tick(self: *Creature) void {
+                for (self.joints) |*joint| {
+                    joint.tick();
+                }
+                for (self.connections) |*connection| {
+                    connection.tick();
+                }
+            }
+            pub fn draw(self: Creature) void {
+                for (self.connections) |connection| {
+                    connection.draw();
+                }
+                for (self.joints) |joint| {
+                    joint.draw();
+                }
+            }
+            pub fn deinit(self: *Creature, allocator: Allocator) void {
+                allocator.free(self.connections);
+                allocator.free(self.joints);
+            }
+        };
+
+        /// joint_amount must be bigger then 1.
+        pub fn makeRandomCreature(joint_amount: u8, allocator: Allocator) !Creature {
+            var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+            defer arena.deinit();
+            const arena_allocator = arena.allocator();
+            assert(joint_amount > 1);
+            const joints = try allocator.alloc(Particle, joint_amount);
+            for (joints) |*joint| {
+                joint.* = Particle{
+                    .pos = r.Vector2{
+                        .x = random.float(f32) * 600,
+                        .y = random.float(f32) * 100,
+                    },
+                    .ball_elasticity = -random.float(f32),
+                };
+            }
+
+            var connections = std.ArrayList([2]usize).init(allocator);
+            defer connections.deinit();
+            var joints_visits = try arena_allocator.alloc(bool, joint_amount);
+
+            @memset(joints_visits, false);
+            var current_joint: usize = 0;
+            var next_joint: usize = 0;
+            var unvisited_num = joint_amount;
+
+            graph_traversal: while (unvisited_num > 0) {
+                current_joint = random.uintLessThan(usize, joints_visits.len);
+
+                if (!joints_visits[current_joint]) {
+                    joints_visits[current_joint] = true;
+                    unvisited_num -= 1;
+                }
+
+                while (next_joint == current_joint) {
+                    next_joint = random.uintLessThan(usize, joints_visits.len);
+                }
+
+                // If the two joints are already connected, skip
+                for (connections.items) |connection| {
+                    if (connection[0] == current_joint and connection[1] == next_joint or
+                        connection[1] == current_joint and connection[0] == next_joint)
+                    {
+                        continue :graph_traversal;
+                    }
+                }
+
+                try connections.append(.{ current_joint, next_joint });
+            }
+            var springs = try allocator.alloc(Spring, connections.items.len);
+            for (connections.items, 0..) |connection, i| {
+                springs[i] = Spring{ .particals = .{ &joints[connection[0]], &joints[connection[1]] } };
+            }
+            return Creature{ .joints = joints, .connections = springs };
+        }
+    };
+}
--- src/main.zig
+++ src/main.zig
@@ -1,65 +1,43 @@
 const r = @import("cHeaders.zig").raylib;
 const std = @import("std");
-const Physics = @import("physics/particle.zig");
 const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
 
 const screenWidth = 800;
 const screenHeight = 450;
 const groundLevel = screenHeight / 2;
-
-var prng = std.Random.DefaultPrng.init(0);
-const random = prng.random();
-
 const gravity = 0.2;
 
-const Particle = Physics.initParticleStruct(groundLevel, gravity);
-const Spring = Physics.initParticleSpring(groundLevel, gravity);
+const creatures = @import("creature.zig").init(groundLevel, gravity);
 
 pub fn main() !void {
-    // var buffer: [10000]u8 = undefined;
-    // var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    // const allocator = fba.allocator();
-    // r.DrawText(@ptrCast(try std.fmt.bufPrintZ(&buffer, "{any}", .{c})), 0, 0, 20, r.LIGHTGRAY);
+    var buffer: [10000]u8 = undefined;
+    var textBuffer: [500]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
 
     r.SetConfigFlags(r.FLAG_MSAA_4X_HINT);
     r.InitWindow(screenWidth, screenHeight, "my window! WOOOOOOOOOOOW!!!!!!");
     defer r.CloseWindow();
     r.SetTargetFPS(60);
 
-    var p1 = Particle{
-        .pos = .{
-            .x = screenWidth / 2 - 100,
-            .y = 300,
-        },
-    };
-    var p2 = Particle{
-        .pos = .{
-            .x = screenWidth / 2,
-            .y = 0,
-        },
-    };
-    var p3 = Particle{
-        .pos = .{
-            .x = screenWidth / 2 + 100,
-            .y = 300,
-        },
-    };
-    var springs = [_]Spring{
-        .{ .particals = .{ &p1, &p2 }, .k = 0.05 },
-        .{ .particals = .{ &p2, &p3 }, .k = 0.05 },
-        .{ .particals = .{ &p3, &p1 }, .k = 0.05 },
-    };
+    var c = try creatures.makeRandomCreature(3, allocator);
+    defer c.deinit(allocator);
 
     while (!r.WindowShouldClose()) // Detect window close button or ESC key
     {
         r.BeginDrawing();
         r.ClearBackground(r.RAYWHITE);
-        for (&springs) |*s| {
-            s.tick();
-            s.draw();
-        }
 
+        c.tick();
+
+        var averagePos: f32 = 0;
+        for (c.joints) |joint| {
+            averagePos += joint.pos.x;
+        }
+        averagePos /= @floatFromInt(c.joints.len);
+
+        c.draw();
+        r.DrawText(@ptrCast(try std.fmt.bufPrintZ(&textBuffer, "creature reached {d} position", .{averagePos})), 0, 0, 20, r.LIGHTGRAY);
         // Draw ground
         r.DrawRectangle(0, groundLevel, screenWidth, screenHeight, r.BLACK);
 
