1ac507e95084f78a7c6de4e218ae403b75c45a0c
Yuval Herman <yuvalherman99@gmail.com>
2024-03-14 12:02:33 +0200

    added signs

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const r = @import("cHeaders.zig").raylib;
 const std = @import("std");
+const bufPrint = std.fmt.bufPrint;
 
 const SCREEN_WIDTH = 1000;
 const SCREEN_HEIGHT = 500;
@@ -10,6 +11,7 @@ const DAMPING = 0.99;
 const Creature = @import("creature.zig").init(GROUND_LEVEL, GRAVITY, DAMPING);
 var prng = std.Random.DefaultPrng.init(0);
 const random = prng.random();
+var textBuffer: [1000]u8 = undefined;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -24,40 +26,58 @@ pub fn main() !void {
         .offset = .{ .x = SCREEN_WIDTH / 2, .y = SCREEN_HEIGHT / 2 },
     };
 
-    var pop: [1000]Creature = undefined;
-    for (&pop) |*c| {
-        c.* = try Creature.createRandom(
-            random.intRangeAtMost(usize, 2, 10),
-            random.float(f32),
-            allocator,
-        );
-    }
+    var c: Creature = try Creature.createRandom(4, 0.5, allocator);
 
-    for (&pop) |*c| {
-        for (0..1000) |_| {
-            c.tick();
-        }
-    }
-
-    var fc: Creature = pop[0];
-    var fp = fc.getFarthestNodePos().x;
-    for (pop) |c| {
-        const cfp = c.getFarthestNodePos().x;
-        if (cfp <= fp) continue;
-        fp = cfp;
-        fc = c;
-    }
     while (!r.WindowShouldClose()) // Detect window close button or ESC key
     {
         r.BeginDrawing();
         r.ClearBackground(r.RAYWHITE);
         r.BeginMode2D(camera);
-        fc.tick();
-        fc.draw();
-        camera.target = fc.nodes.items[0].pos;
 
-        r.DrawRectangle(@intFromFloat(camera.target.x - SCREEN_WIDTH / 2 - 10), GROUND_LEVEL, SCREEN_WIDTH + 20, 500, r.BLACK);
+        var text_buffer_idx: usize = 0;
+        for (0..100) |i| {
+            const font_size = 30;
+            var sign = r.Rectangle{
+                .x = @floatFromInt(i * 250),
+                .y = GROUND_LEVEL - 150,
+                .height = 50,
+                .width = undefined,
+            };
+            const text_slice = try bufPrint(textBuffer[text_buffer_idx..], "{d:.1}", .{sign.x / 100});
+            text_buffer_idx += text_slice.len + 1; // add one to account for c-style null termination.
+            const text: [*c]u8 = @ptrCast(text_slice);
+            const text_width: f32 = @floatFromInt(r.MeasureText(text, font_size));
+            sign.width = @max(100, text_width + 20);
+            r.DrawRectangleRec(sign, r.LIGHTGRAY);
+            r.DrawText(
+                text,
+                @intFromFloat(sign.x + sign.width / 2 - text_width / 2),
+                @intFromFloat(sign.y + sign.height / 2 - font_size / 2),
+                font_size,
+                r.BLACK,
+            );
+        }
+
+        // c.mutate();
+        c.tick();
+        c.draw();
+
+        camera.target.x = 0;
+        for (c.nodes.items) |n| {
+            camera.target.x += n.pos.x;
+        }
+        camera.target.x /= @floatFromInt(c.nodes.items.len);
+
+        camera.target.y = GROUND_LEVEL;
+
+        r.DrawRectangle(@intFromFloat(camera.target.x - SCREEN_WIDTH), GROUND_LEVEL, SCREEN_WIDTH * 2, 500, r.BLACK);
+
         r.EndMode2D();
         r.EndDrawing();
     }
+}
+
+fn creatureComp(context: void, a: Creature, b: Creature) bool {
+    _ = context;
+    return a.fitness > b.fitness;
 }
