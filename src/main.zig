81a86dbb7b0c6259aabea15bda166d89faab1e02
Yuval Herman <yuvalherman99@gmail.com>
2024-03-17 14:51:02 +0200

    adapt main to vectors

--- src/main.zig
+++ src/main.zig
@@ -6,21 +6,26 @@ const bufPrint = std.fmt.bufPrint;
 const SCREEN_WIDTH = 1000;
 const SCREEN_HEIGHT = 500;
 const GROUND_LEVEL = SCREEN_HEIGHT / 2 - 100;
+
 const GRAVITY = 0.7;
 const DAMPING = 0.99;
+
 const EVALUATION_TICKS = 60 * 10;
+const RELAX_GRAPH_ITERS = 100;
+
 const SELECTION_RATE = 10;
 const MUTATION_RATE = 0.1;
 const IND_MUTATION_RATE = 0.5;
-const GENERATIONS = 50;
-const RELAX_GRAPH_ITERS = 100;
+
 const POPULATION_SIZE = 1000;
+const GENERATIONS = 50;
 const HOF_SIZE = 1;
 
 const Creature = @import("creature.zig").init(GROUND_LEVEL, GRAVITY, DAMPING, RELAX_GRAPH_ITERS);
 var prng = std.Random.DefaultPrng.init(0);
 const random = prng.random();
 var textBuffer: [10000]u8 = undefined;
+const Timer = std.time.Timer;
 
 pub fn main() !void {
     r.SetTraceLogLevel(r.LOG_WARNING);
@@ -49,10 +54,21 @@ pub fn main() !void {
         c.* = try Creature.createRandom(random.intRangeAtMost(usize, 3, 5), random.float(f32), allocator);
     }
 
+    var t = try Timer.start();
+    const Timings = struct {
+        evaluate: u64 = 0,
+        mutate: u64 = 0,
+        crossover: u64 = 0,
+    };
+    var timings = Timings{};
+
     for (0..GENERATIONS) |gen| {
+        t.reset();
         for (pop[HOF_SIZE..]) |*c| {
             c.evaluate(EVALUATION_TICKS);
         }
+        timings.evaluate += t.read();
+
         std.mem.sort(Creature, &pop, {}, creatureComp);
         best_history[gen] = try pop[0].clone();
 
@@ -66,15 +82,17 @@ pub fn main() !void {
         avg_nodes /= pop.len;
 
         const avg = pop[0].getAvgPos();
-        std.debug.print("gen: {}, best avg: ({d:.2}, {d:.2}), best fitness:{d:.2}, edges: {}, nodes: {}\n", .{ gen, avg.x, avg.y, pop[0].fitness, avg_edges, avg_nodes });
+        std.debug.print("gen: {}, best avg: ({d:.2}, {d:.2}), best fitness:{d:.2}, edges: {}, nodes: {}\n", .{ gen, avg[0], avg[1], pop[0].fitness, avg_edges, avg_nodes });
 
         for (&newPop) |*nc| {
             const a = random.intRangeLessThan(usize, 0, pop.len / SELECTION_RATE);
             var b = a;
             while (a == b) b = random.intRangeLessThan(usize, 0, pop.len / SELECTION_RATE);
+            t.reset();
             nc.* = try pop[a].crossover(pop[b]);
-
+            timings.crossover += t.lap();
             if (random.float(f32) < MUTATION_RATE) try nc.mutate(IND_MUTATION_RATE);
+            timings.mutate += t.read();
         }
 
         for (pop[HOF_SIZE..]) |*c| {
@@ -86,7 +104,11 @@ pub fn main() !void {
         c.evaluate(EVALUATION_TICKS);
     }
     std.mem.sort(Creature, &pop, {}, creatureComp);
-
+    std.debug.print("evaluate: {d:.2} crossover: {d:.2} mutate: {d:.2}\n", .{
+        @as(f64, @floatFromInt(timings.evaluate)) * 1e-9,
+        @as(f64, @floatFromInt(timings.crossover)) * 1e-9,
+        @as(f64, @floatFromInt(timings.mutate)) * 1e-9,
+    });
     var camera = r.Camera2D{
         .zoom = 1,
         .offset = .{ .x = SCREEN_WIDTH / 2, .y = SCREEN_HEIGHT / 2 },
@@ -116,7 +138,7 @@ pub fn main() !void {
             changeC = false;
             c = best_history[cI];
             const avg = c.getAvgPos();
-            std.debug.print("timer: {}, cI: {} avg: ({d:.2}, {d:.2})\n", .{ timer, cI, avg.x, avg.y });
+            std.debug.print("timer: {}, cI: {} avg: ({d:.2}, {d:.2})\n", .{ timer, cI, avg[0], avg[1] });
             c.resetValues();
         }
         r.BeginDrawing();
@@ -151,7 +173,9 @@ pub fn main() !void {
         c.tick();
         c.draw();
 
-        camera.target = c.getAvgPos();
+        const avg_pos = c.getAvgPos();
+        camera.target.x = avg_pos[0];
+        camera.target.y = avg_pos[1];
 
         text_slice = try bufPrint(textBuffer[text_buffer_idx..], "{d:.1}", .{camera.target.x});
         text_buffer_idx += text_slice.len + 1; // add one to account for c-style null termination.
